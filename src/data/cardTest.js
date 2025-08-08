26faa23aea21b3d88b3da04319f49361f1ca8f56
Diego Caucota <diegocaucota@gmail.com>
2025-08-08 11:06:29 -0300

    add: components data cardTest

---
+++ src/data/cardTest.js
@@ -0,0 +1,258 @@
+export const cardsData = [
+  {
+    id: 1,
+    title: "Avengers: Infinity War",
+    image:
+      "https://www.movieposters.com/cdn/shop/products/INFINITY-WAR-LEGAL_480x.progressive.jpg?v=1666272866",
+    description_breve:
+      "Thanos amenaza con borrar la mitad del universo. Los Avengers deben unirse para detenerlo en una lucha por la supervivencia.",
+    description_amplia:
+      "Thanos busca reunir las Gemas del Infinito para equilibrar el universo. Los Avengers, desde Wakanda hasta el espacio, libran una batalla épica que marcará un antes y un después.",
+    alt: "Póster oficial de Avengers: Infinity War",
+    type: "Pelicula",
+    genre: "Acción",
+    year: 2018,
+    director: "Anthony y Joe Russo",
+    trailer: "https://www.youtube.com/watch?v=6ZfuNTqbHE8",
+    destacada: true,
+  },
+  {
+    id: 2,
+    title: "Avengers: Endgame",
+    image:
+      "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/108b520c55e3c9760f77a06110d6a73b_e97cf224-d57f-44e3-8477-4f5479cd746b_500x749.jpg?v=1573616089.jpg",
+    description_breve:
+      "El cierre de una era. Los Avengers intentan revertir el chasquido de Thanos en una misión que pone a prueba el tiempo y la amistad.",
+    description_amplia:
+      "Tras el desastre causado por Thanos, los héroes sobrevivientes planean viajar en el tiempo para deshacerlo. Una batalla final reúne a todos los personajes del UCM en un enfrentamiento épico y emotivo.",
+    type: "Pelicula",
+    genre: "Acción",
+    year: 2019,
+    director: "Anthony y Joe Russo",
+    trailer: "https://www.youtube.com/watch?v=TcMBFSGVi1c",
+    destacada: false,
+  },
+  {
+    id: 3,
+    title: "Matrix",
+    image:
+      "https://www.movieposters.com/cdn/shop/products/ed4796ac6feff9d2a6115406f964c928_6b200bda-fe71-4900-ad7f-903cdda50dab_480x.progressive.jpg?v=1573587596",
+    description_breve:
+      "Neo descubre que el mundo es una simulación controlada por máquinas y se une a la rebelión humana.",
+    description_amplia:
+      "Thomas Anderson, alias Neo, se enfrenta a la verdad: el mundo real está oculto tras Matrix, una simulación creada por máquinas. Guiado por Morpheus, se convierte en el elegido para liberar a la humanidad.",
+    alt: "Póster de Matrix",
+    type: "Pelicula",
+    genre: "Ciencia ficción",
+    year: 1999,
+    director: "Lana y Lilly Wachowski",
+    trailer: "https://www.youtube.com/watch?v=vKQi3bBA1y8",
+    destacada: false,
+  },
+  {
+    id: 4,
+    title: "El Padrino",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    description_breve:
+      "El ascenso de Michael Corleone, de hijo idealista a jefe mafioso implacable, en una saga familiar inolvidable.",
+    description_amplia:
+      "Michael Corleone, hijo de Don Vito, es arrastrado al negocio familiar tras un atentado. Su evolución de civil a líder mafioso marca una transformación trágica en esta obra maestra del cine.",
+    alt: "Póster de El Padrino",
+    type: "Pelicula",
+    genre: "Drama",
+    year: 1972,
+    director: "Francis Ford Coppola",
+    trailer: "https://www.youtube.com/watch?v=sY1S34973zA",
+    destacada: false,
+  },
+  {
+    id: 5,
+    title: "Interestelar",
+    image:
+      "https://m.media-amazon.com/images/I/91obuWzA3XL._AC_UF1000,1000_QL80_.jpg",
+    description_breve:
+      "Un piloto viaja por el espacio para salvar a la humanidad mientras enfrenta los límites del tiempo y la física.",
+    description_amplia:
+      "En busca de un nuevo planeta habitable, Cooper lidera una misión interestelar. La ciencia, el tiempo y el amor se entrelazan en una travesía visual y emocional que desafía las leyes del universo.",
+    alt: "Póster de Interestelar",
+    type: "Pelicula",
+    genre: "Ciencia ficción",
+    year: 2014,
+    director: "Christopher Nolan",
+    trailer: "https://www.youtube.com/watch?v=UoSSbmD9vqc",
+    destacada: false,
+  },
+  {
+    id: 6,
+    title: "Parásitos",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
+    description_breve:
+      "Una familia pobre se infiltra en la casa de una familia rica, desatando una oscura y satírica lucha de clases.",
+    description_amplia:
+      "Los Kim se introducen en la vida de los Park fingiendo ser empleados calificados. Pero los secretos y tensiones sociales desencadenan una historia impredecible con crítica social y suspenso.",
+    alt: "Póster de Parásitos",
+    type: "Pelicula",
+    genre: "Terror",
+    year: 2019,
+    director: "Bong Joon-ho",
+    trailer: "https://www.youtube.com/watch?v=isOGD_7hNIY",
+    destacada: false,
+  },
+  {
+    id: 7,
+    title: "Pulp Fiction",
+    image:
+      "https://m.media-amazon.com/images/I/71c05lTE03L._AC_UF1000,1000_QL80_.jpg",
+    description_breve:
+      "Gánsters, boxeadores y un maletín misterioso se cruzan en este clásico no lineal lleno de humor y violencia.",
+    description_amplia:
+      "Tarantino entrelaza historias de criminales, boxeadores y un maletín en un relato no lineal. Humor negro, diálogos brillantes y escenas icónicas que marcaron un hito en el cine moderno.",
+    alt: "Póster de Pulp Fiction",
+    type: "Pelicula",
+    genre: "Crimen",
+    year: 1994,
+    director: "Quentin Tarantino",
+    trailer: "https://www.youtube.com/watch?v=s7EdQ4FqbhY",
+    destacada: false,
+  },
+  {
+    id: 8,
+    title: "The Shawshank Redemption",
+    image: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
+    description_breve:
+      "Una prisión, un inocente y un plan que cambiará vidas. Shawshank es más que rejas: es la historia de cómo la esperanza escapa donde todo parece perdido.",
+    description_amplia:
+      "Andy Dufresne es enviado a Shawshank por un crimen que no cometió. Mientras los años pasan, construye amistades profundas, especialmente con Red, y encuentra formas ingeniosas de resistir el sistema desde dentro. Crea una biblioteca, ayuda a los guardias con finanzas y, en secreto, diseña su liberación. Una historia de redención que demuestra cómo la resiliencia y la esperanza pueden abrir túneles hacia la libertad.",
+    alt: "Póster de The Shawshank Redemption",
+    type: "Pelicula",
+    genre: "Drama",
+    year: 1994,
+    director: "Frank Darabont",
+    trailer: "https://www.youtube.com/watch?v=6hB3S9bIaco",
+    destacada: false,
+  },
+  // series
+  {
+    id: 9,
+    title: "Peaky Blinders",
+    image:
+      "https://www.movieposters.com/cdn/shop/products/peakyblinders_480x.progressive.jpg?v=1620153863",
+    description_breve:
+      "Sombreros afilados, negocios sucios. En el corazón de Birmingham, los Shelby reinan con puños de hierro y trajes impecables.",
+    description_amplia:
+      "Después de la Gran Guerra, Thomas Shelby regresa a casa con cicatrices físicas y mentales. Lidera a su familia, los Peaky Blinders, en un mundo de apuestas, contrabando y política corrupta. Su inteligencia fría y ambición sin límites lo enfrentan con enemigos poderosos. En un clima de tensión constante, la serie mezcla estética estilizada, violencia estratégica y una narrativa densa sobre poder, identidad y legado.",
+    alt: "Póster oficial de la serie Peaky Blinders",
+    type: "Serie",
+    genre: "Crimen",
+    year: 2013,
+    director: "Steven Knight",
+    trailer: "https://www.youtube.com/watch?v=oVzVdvGIC7U",
+    destacada: false,
+  },
+  {
+    id: 10,
+    title: "Vikings",
+    image:
+      "https://i.pinimg.com/474x/c2/5c/95/c25c95916c0f9390913cf3b1a04d62e9.jpg",
+    description_breve:
+      "Desde Kattegat al fin del mundo. Ragnar desafía dioses y reyes en una saga de conquista, fe y destino.",
+    description_amplia:
+      "Ragnar Lothbrok, impulsado por visiones divinas y sed de gloria, desafía la tradición para explorar nuevas tierras. Su viaje lo lleva a batallas sangrientas, alianzas traicioneras y conflictos internos con su familia. Con un enfoque crudo y espiritual, la serie retrata la expansión vikinga y cómo el precio del poder transforma al hombre y a su linaje. Mitología, política y brutalidad se entrelazan en una epopeya sin igual.",
+    alt: "Póster oficial de la serie Vikings",
+    type: "Serie",
+    genre: "Drama",
+    year: 2013,
+    director: "Michael Hirst",
+    trailer: "https://www.youtube.com/watch?v=2HeycBS4edw",
+    destacada: false,
+  },
+  {
+    id: 11,
+    title: "Game of Thrones",
+    image:
+      "https://www.movieposters.com/cdn/shop/files/game-of-thrones_480x.progressive.jpg?v=1682430369",
+    description_breve:
+      "Siete reinos, una guerra sin reglas. En Poniente, cada alianza se forja con sangre y cada trono se gana con traición.",
+    description_amplia:
+      "En una tierra dividida por ambición, traición y antiguas profecías, nobles familias compiten por el Trono de Hierro. Los Stark, los Lannister, los Targaryen y otros se enfrentan en una red de conspiraciones que trasciende generaciones. Mientras tanto, un peligro sobrenatural amenaza con destruir a todos por igual. 'Game of Thrones' es una obra maestra de drama, fantasía y brutal realismo, donde ningún personaje está a salvo y el invierno nunca está tan lejos como parece.",
+    alt: "Póster oficial de Game of Thrones temporada 8",
+    type: "Serie",
+    genre: "Fantasía",
+    year: 2011,
+    director: "David Benioff & D. B. Weiss (showrunners)",
+    trailer: "https://www.youtube.com/watch?v=IznyEBoUcuk",
+    destacada: false,
+  },
+  {
+    id: 12,
+    title: "El juego del calamar 3",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_617665-MLA80194644714_112024-O.webp",
+    description_breve:
+      "Un nuevo juego, mismas reglas mortales. La tercera ronda eleva el riesgo en un mundo donde el dinero vale más que la vida.",
+    description_amplia:
+      "En esta nueva entrega del fenómeno coreano, los participantes —nuevos y viejos— son arrastrados a versiones más complejas y letales de juegos infantiles. A medida que se revelan las verdaderas motivaciones detrás de la organización, Gi Seok y otros deben enfrentarse no solo a la muerte, sino también a dilemas éticos y traiciones inesperadas. Con escenarios aún más elaborados y un enfoque en la psicología del juego, esta temporada lleva la crítica social a un nuevo nivel de tensión y reflexión.",
+    alt: "Póster oficial de la serie El juego del calamar",
+    type: "Serie",
+    genre: "Drama",
+    year: 2021,
+    director: "Hwang Dong-hyuk",
+    trailer: "https://www.youtube.com/watch?v=cTpISvwP9DM",
+    destacada: false,
+  },
+  // surtidas
+  {
+    id: 13,
+    title: "The Avengers",
+    image:
+      "https://www.movieposters.com/cdn/shop/files/avengers.24x36_480x.progressive.jpg?v=1707410714",
+    description_breve:
+      "Cuando la amenaza es global, los héroes más poderosos del planeta deben actuar como uno solo. Nace un equipo, nace una leyenda.",
+    description_amplia:
+      "Loki desata el caos al controlar un artefacto de poder ilimitado, obligando a Nick Fury a reunir a un grupo dispar de héroes. A pesar de sus conflictos personales, Iron Man, Thor, Hulk y los demás aprenden a luchar como uno solo. Con humor, acción explosiva y escenas icónicas, 'The Avengers' es el punto de inflexión del Universo Cinematográfico de Marvel: el momento donde lo imposible se hizo inevitable.",
+    alt: "Póster oficial de The Avengers",
+    type: "Pelicula",
+    genre: "Acción",
+    year: 2012,
+    director: "Joss Whedon",
+    trailer: "https://www.youtube.com/watch?v=eOrNdBpGMv8",
+    destacada: false,
+  },
+  {
+    id: 14,
+    title: "Minions",
+    image:
+      "https://www.movieposters.com/cdn/shop/products/8411d41e7037095963a2d2402daa0fa0_480x.progressive.jpg?v=1573572677",
+    description_breve:
+      "Antes de ser esbirros de Gru, los Minions estaban en busca del jefe más malvado. Y su viaje... es un completo caos adorable.",
+    description_amplia:
+      "Los Minions han servido a los villanos más desastrosos de la historia, pero tras quedarse sin amo, tres de ellos —Kevin, Stuart y Bob— emprenden una misión para encontrar a un nuevo líder. Su búsqueda los lleva hasta Scarlett Overkill, una ambiciosa supervillana. En medio de errores hilarantes y gadgets imposibles, esta precuela nos muestra el origen del caos amarillo que conquistó al mundo. Repleta de humor físico, estética retro y momentos entrañables.",
+    alt: "Póster oficial de Minions",
+    type: "Pelicula",
+    genre: "Animación",
+    year: 2015,
+    director: "Pierre Coffin, Kyle Balda",
+    trailer: "https://www.youtube.com/watch?v=LSw_te6wexM",
+    destacada: false,
+  },
+
+  {
+    id: 15,
+    title: "Los Croods",
+    image: "https://m.media-amazon.com/images/I/71nmuZLbRqL.jpg",
+    description_breve:
+      "Para sobrevivir, deben evolucionar. Los Croods descubren que el cambio no siempre es peligroso... a veces, es lo mejor que puede pasar.",
+
+    description_amplia:
+      "Grug Crood protege ferozmente a su familia del peligro exterior, pero cuando su mundo colapsa —literalmente—, deben aventurarse en lo desconocido. Guiados por Guy, un joven con ideas revolucionarias, descubren nuevas tierras, extrañas criaturas y formas de pensar diferentes. En este viaje, aprenderán que adaptarse no es traicionar el pasado, sino abrazar el futuro. Una historia de humor, emoción y evolución contada con colorido desbordante y corazón prehistórico.",
+    alt: "Póster oficial de Los Croods",
+    type: "Pelicula",
+    genre: "Animación",
+    year: 2013,
+    director: "Kirk DeMicco, Chris Sanders",
+    trailer: "https://www.youtube.com/watch?v=lNk1XIejGRA",
+    destacada: false,
+  },
+];
