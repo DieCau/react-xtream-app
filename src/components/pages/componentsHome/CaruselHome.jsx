a01373e5a28947f759eaf686c7b210b8b1eb2dd5
Diego Caucota <diegocaucota@gmail.com>
2025-08-12 10:51:00 -0300

    fix: CaruselHome format

--- src/components/pages/componentsHome/CaruselHome.jsx
+++ src/components/pages/componentsHome/CaruselHome.jsx
@@ -1,5 +1,5 @@
-import Carousel from 'react-bootstrap/Carousel';
-import '../../../index.css';
+import Carousel from "react-bootstrap/Carousel";
+import "../../../index.css";
 
 // Componente de carrusel para la página de inicio
 // Muestra imágenes destacadas de la plataforma
@@ -11,16 +11,16 @@ const CaruselHome = () => {
       <Carousel>
         <Carousel.Item>
           <img
-            className='w-100 img-fluid'
-            style={{ maxHeight: '400px', objectFit: 'cover' }}
+            className="w-100 img-fluid"
+            style={{ maxHeight: "400px", objectFit: "cover" }}
             src="https://disneyplusbrasil.com.br/wp-content/uploads/2020/11/perfil-comum-1024x487.jpg"
             alt="perfil de usuarios con animaciones "
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className='w-100 img-fluid'
-            style={{ maxHeight: '400px', objectFit: 'cover' }}
+            className="w-100 img-fluid"
+            style={{ maxHeight: "400px", objectFit: "cover" }}
             src="https://telaviva.com.br/wp-content/uploads/2022/05/streaming.jpg"
             alt="dinujos animados de peliculas y cine para compartir en familia"
           />
