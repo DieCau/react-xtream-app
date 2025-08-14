5ffeb57eacc3ef41a90bdbe41ea8eee0a849c989
Diego Caucota <diegocaucota@gmail.com>
2025-08-13 21:48:17 -0300

    fix: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Crea un contenedor de raíz de React y renderiza el componente App
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
